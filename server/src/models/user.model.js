b07ba0d08468bb906bf683cb14c88c6076795cfa
Abduselam Awel <abdselam676@gmail.com>
2026-06-25 00:55:00 +0300

    User model has been created

---
+++ server/src/models/user.model.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    wishlist: [],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = model(userSchema, "User");
+
+module.exports = User;
